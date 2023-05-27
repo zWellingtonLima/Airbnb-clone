deee1edace4af8386ee106ca6f7eeca1f595bc44
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-05-27 16:12:59 +0100

    CategoryInput added

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -4,6 +4,8 @@ import useRentModal from "@/app/hooks/useRentModal copy";
 import Modal from "./Modal";
 import { useMemo, useState } from "react";
 import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
 
 enum STEPS {
   CATEGORY = 0,
@@ -50,6 +52,18 @@ const RentModal = () => {
         title="Which of these best describes your place?"
         subtitle="Pick a category"
       />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
+        {categories.map((item) => (
+          <div key={item.label} className="col-span-1">
+            <CategoryInput 
+              onClick={() => {}}
+              selected={false}
+              label={item.label}
+              icon={item.icon}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   )
 
