85bf80a6df7f364f0a288690a43c900907db70c7
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-05-31 17:43:02 +0100

    Step2 - location selection, map component added.

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,7 @@ import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
+import Map from "../Map";
 
 enum STEPS {
   CATEGORY = 0,
@@ -45,6 +46,7 @@ const RentModal = () => {
   })
 
   const category = watch('category')
+  const location = watch('location')
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -107,7 +109,11 @@ const RentModal = () => {
           title="Where is your place located?"
           subtitle="Help guests find you!"
         />
-        <CountrySelect />
+        <CountrySelect
+          value={location} 
+          onChange={(value) => setCustomValue('location', value)}
+        />
+        <Map />
       </div>
     )
   }
