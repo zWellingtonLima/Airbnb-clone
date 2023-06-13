19ff011d5f14b6cb0510b0330b9777e3acb08470
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-06-13 21:08:48 +0100

    useFavorite Hook added.

---
+++ app/hooks/useFavorite.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
+import { toast } from "react-hot-toast";
+
+import { SafeUser } from "../types";
+import useLoginModal from "./useLoginModal";
+
+interface IUseFavorite {
+  listingId: string;
+  currentUser?: SafeUser | null;
+}
+
+const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
+  const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const hasFavorite = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
+
+    return list.includes(listingId);
+  }, [currentUser, listingId]);
+
+  const toggleFavorite = useCallback(async () => {
+
+  }, [])
+};
