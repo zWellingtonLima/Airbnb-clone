064892440fd62de42346ce79d4121f2b7f7f2209
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-06-17 08:40:01 +0100

    ListingClient, ListingCategory, ListingHead and ListingInfo added

---
+++ app/components/listings/ListingCategory.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { FC } from "react"
+
+interface ListingCategoryProps {
+  icon: {
+
+  }
+  label: string
+  description: string
+}
+
+const ListingCategory: FC<ListingCategoryProps> = ({}) => {
+  return <div>ListingCategory</div>
+}
+
+export default ListingCategory
--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,5 +1,27 @@
-const ListingPage = () => {
-  return <div>ListingPage</div>
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getListingById from "@/app/actions/getListingById";
+import EmptyState from "@/app/components/EmptyState";
+import ListingClient from "./ListingClient";
+
+interface IParams {
+  listingId: string;
 }
 
-export default ListingPage
+const ListingPage = async ({ params }: { params: IParams }) => {
+  const listing = await getListingById(params);
+  const currentUser = await getCurrentUser();
+
+  if (!listing) {
+    return <EmptyState />;
+  }
+
+  return (
+    <>
+      <ListingClient 
+        listing={listing} 
+        currentUser={currentUser} />
+    </>
+  );
+};
+
+export default ListingPage;
