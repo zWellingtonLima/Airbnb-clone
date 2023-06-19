301fbd29281fc37a698dc0fd2598feef0ac86c6a
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-06-19 20:32:26 +0100

    Calendar created on ListingClient.tsx

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { FC } from "react";
 
-import { Range } from 'react-date-range'
+import { Range } from "react-date-range";
+import Calendar from "../inputs/Calendar";
 
 interface ListingReservationProps {
   price: number;
@@ -21,7 +24,21 @@ const ListingReservation: FC<ListingReservationProps> = ({
   disabled,
   disabledDates,
 }) => {
-  return <div>ListingReservation</div>;
+  return (
+    <div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden">
+      <div className="flex flex-row items-center gap-1 p-4">
+        <div className="text-2xl font-semibold">$ {price}</div>
+        <div className="font-light text-neutral-600">night</div>
+      </div>
+      <hr />
+
+      <Calendar
+        value={dateRange}
+        disabledDates={disabledDates}
+        onChange={(value) => onChangeDate(value.selection)}
+      />
+    </div>
+  );
 };
 
 export default ListingReservation;
