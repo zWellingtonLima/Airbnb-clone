c12f78524a4c08d0ea777a2349bc2419ee280f6a
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-06-26 21:11:24 +0100

    SearchModal done. params passed to URL.

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -11,6 +11,7 @@ import CountrySelect, { CountrySelectValue } from "../inputs/CountrySelect";
 import { formatISO } from "date-fns";
 import Heading from "../Heading";
 import Calendar from "../inputs/Calendar";
+import Counter from "../inputs/Counter";
 
 enum STEPS {
   LOCATION = 0,
@@ -139,13 +140,44 @@ const SearchModal = () => {
     )
   }
 
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading 
+          title="More information"
+          subtitle="Find your perfect place!"
+        />
+        <Counter 
+          title="Guests"
+          subtitle="How many guests are coming?"
+          value={guestCount}
+          onChange={(value) => setGuestCount(value)}
+        />
+        <Counter 
+          title="Rooms"
+          subtitle="How many do you need?"
+          value={roomCount}
+          onChange={(value) => setRoomCount(value)}
+        />
+        <Counter 
+          title="Bathrooms"
+          subtitle="How many bathrooms do you need?"
+          value={bathroomCount}
+          onChange={(value) => setBathroomCount(value)}
+        />
+      </div>
+    )
+  }
+
   return (
     <Modal
       isOpen={searchModal.isOpen}
       onClose={searchModal.onClose}
-      onSubmit={searchModal.onOpen}
+      onSubmit={onSubmit}
       title="Filters"
-      actionLabel="Search"
+      actionLabel={actionLabel}
+      secondaryAction={step === STEPS.LOCATION ? undefined : onBack}
+      secondaryActionLabel={secondaryActionLabel}
       body={bodyContent}
     />
   );
