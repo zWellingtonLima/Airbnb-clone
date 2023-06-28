04dc87ad288c15a8b48dcb3df6a6d049af3bba6c
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-06-28 20:33:13 +0100

    Added middleware.ts to protect some paths

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -27,19 +27,27 @@ const Search = () => {
 
   const durationLabel = useMemo(() => {
     if (startDate && endDate) {
-      const start = new Date(startDate as string)
-      const end = new Date(endDate as string)
-      let diff = differenceInDays(end, start)
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+      let diff = differenceInDays(end, start);
 
       if (diff === 0) {
-        diff = 1
+        diff = 1;
       }
 
-      return `${diff} Days`
+      return `${diff} Days`;
     }
 
-    return 'Any Week'
-  }, [startDate, endDate])
+    return "Any Week";
+  }, [startDate, endDate]);
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`;
+    }
+
+    return "Add Guests";
+  }, [guestCount]);
 
   return (
     <div
@@ -47,12 +55,12 @@ const Search = () => {
       className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer"
     >
       <div className="flex flex-row justify-between items-center">
-        <div className="text-sm font-semibold px-6">Anywhere</div>
+        <div className="text-sm font-semibold px-6">{locationLabel}</div>
         <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
-          Any Week
+          {durationLabel}
         </div>
         <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-          <div className="hidden sm:block">Add Guests</div>
+          <div className="hidden sm:block">{guestLabel}</div>
           <div className="p-2 bg-rose-500 rounded-full text-white">
             <BiSearch />
           </div>
